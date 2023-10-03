a299d2d67331d33bbc21a312c33528796933dde2
samblooms <16036598+samblooms@users.noreply.github.com>
2023-10-03 16:25:25 -0500

    make doGet() method in servlet class display HelloWorld webpage

--- src/main/java/Inventory_Management_Servlet.java
+++ src/main/java/Inventory_Management_Servlet.java
@@ -7,32 +7,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Inventory_Management_Servlet
- */
+
 @WebServlet("/Inventory_Management_Servlet")
 public class Inventory_Management_Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Inventory_Management_Servlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set the content type to HTML as we will be outputting HTML content
+        response.setContentType("text/html");
+        
+        // Obtain the PrintWriter object from response to write the HTML content.
+        java.io.PrintWriter out = response.getWriter();
+        
+        // Writing "Hello, World!" HTML content to response
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello, World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
